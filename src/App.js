d64aea87e1ea71ee081ef4b28d90c88d082d3c72
Olorunda_Samuel <olorundasamuel2@gmail.com>
2021-01-26 21:54:58 +0100

    Still Incomplet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Main from "./Components/Main/Main"
+// import logo from './logo.svg';
+// import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Content/ContentPartTwo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import zee from '../Img/zee.png'
+import ddd from '../Img/ddd.gif'
+
+function ContentPartTwo() {
+  return (
+    <div>
+      <div>
+        <img src={zee} alt="" />
+        <img src={ddd} alt="" width="500px" />
+      </div>
+    </div>
+  )
+}
+
+export default ContentPartTwo
